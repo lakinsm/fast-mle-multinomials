Fix to Newton Raphson step computation

multi_mle/blm.py
@@ -98,7 +98,7 @@ def blm_hessian_precompute(U, v_d, v_d1, theta):
                 gradient[d] += (U[d][z] * ((theta[d] + z)**-1))
                 h_diag[d] -= (U[d][z] * ((theta[d] + z)**-2))
             gradient[d] -= (v_d[z] * ((sum_theta + z)**-1))
-            h_diag[d] += (v_d[z] * ((sum_theta + z)**-2))
+            # h_diag[d] += (v_d[z] * ((sum_theta + z)**-2))
         lprob -= v_d[z] * np.log(sum_theta + z)
 
         # beta parameter
@@ -107,10 +107,11 @@ def blm_hessian_precompute(U, v_d, v_d1, theta):
             gradient[-2] += (U[D1-1][z] * ((theta[-2] + z)**-1))
             h_diag[-2] -= (U[D1-1][z] * ((theta[-2] + z)**-2))
         gradient[-2] -= (v_d1[z] * ((theta[-2] + theta[-1] + z)**-1))
-        h_diag[-2] += (v_d1[z] * ((theta[-2] + theta[-1] + z)**-2))
+        # h_diag[-2] += (v_d1[z] * ((theta[-2] + theta[-1] + z)**-2))
 
         # alpha parameter
-        h_diag[-1] += (v_d1[z] * ((theta[-2] + theta[-1] + z)**-2)) - (v_d[z] * ((theta[-1] + z)**-2))
+        # h_diag[-1] += (v_d1[z] * ((theta[-2] + theta[-1] + z)**-2)) - (v_d[z] * ((theta[-1] + z)**-2))
+        h_diag[-1] -= (v_d[z] * ((theta[-1] + z) ** -2))
         gradient[-1] += (v_d[z] * ((theta[-1] + z)**-1)) - (v_d1[z] * ((theta[-2] + theta[-1] + z) ** -1))
         lprob += (v_d[z] * np.log(theta[-1] + z)) - (v_d1[z] * np.log(theta[-2] + theta[-1] + z))
 
@@ -126,12 +127,12 @@ def blm_hessian_precompute(U, v_d, v_d1, theta):
             gradient[-2] += (U[D1 - 1][z] * ((theta[-2] + z) ** -1))
             h_diag[-2] -= (U[D1 - 1][z] * ((theta[-2] + z) ** -2))
         gradient[-2] -= (v_d1[z] * ((theta[-2] + theta[-1] + z) ** -1))
-        h_diag[-2] += (v_d1[z] * ((theta[-2] + theta[-1] + z) ** -2))
+        # h_diag[-2] += (v_d1[z] * ((theta[-2] + theta[-1] + z) ** -2))
 
         # alpha parameter
         gradient[-1] -= v_d1[z] * ((theta[-2] + theta[-1] + z) ** -1)
         lprob -= v_d1[z] * np.log(theta[-2] + theta[-1] + z)
-        h_diag[-1] += (v_d1[z] * ((theta[-2] + theta[-1] + z) ** -2))
+        # h_diag[-1] += (v_d1[z] * ((theta[-2] + theta[-1] + z) ** -2))
 
         # constants
         constants[1] += v_d1[z] * ((theta[-2] + theta[-1] + z)**-2)

multi_mle/dm.py
@@ -78,7 +78,7 @@ def dm_hessian_precompute(U, v, theta):
                 gradient[d] += (U[d][z] * ((theta[d] + z)**-1))
                 h_diag[d] -= (U[d][z] * ((theta[d] + z)**-2))
             gradient[d] -= (v[z] * ((sum_theta + z)**-1))
-            h_diag[d] += (v[z] * ((sum_theta + z)**-2))
+            # h_diag[d] += (v[z] * ((sum_theta + z)**-2))
         constant += v[z] * ((sum_theta + z)**-2)
         lprob -= v[z] * np.log(sum_theta + z)
     return gradient, h_diag, constant, lprob

multi_mle/gdm.py
@@ -97,16 +97,16 @@ def gdm_hessian_precompute(U, vd, theta):
                 lprob -= vd[d][z] * np.log(theta[alpha_idx] + theta[alpha_idx + 1] + z)  # alpha_d + beta_d
                 gradient[alpha_idx] -= vd[d][z] * ((theta[alpha_idx] + theta[alpha_idx + 1] + z)**-1)  # alpha_d
                 gradient[alpha_idx + 1] -= vd[d][z] * ((theta[alpha_idx] + theta[alpha_idx + 1] + z)**-1)  # beta_d
-                h_diag[alpha_idx] += vd[d][z] * ((theta[alpha_idx] + theta[alpha_idx + 1] + z)**-2)  # alpha_d
-                h_diag[alpha_idx + 1] += vd[d][z] * ((theta[alpha_idx] + theta[alpha_idx + 1] + z)**-2)  # beta_d
+                # h_diag[alpha_idx] += vd[d][z] * ((theta[alpha_idx] + theta[alpha_idx + 1] + z)**-2)  # alpha_d
+                # h_diag[alpha_idx + 1] += vd[d][z] * ((theta[alpha_idx] + theta[alpha_idx + 1] + z)**-2)  # beta_d
                 constants[d] += vd[d][z] * ((theta[alpha_idx] + theta[alpha_idx + 1] + z) ** -2)
         alpha_idx = D
         if z < len(vd[D]):
             lprob -= vd[D][z] * np.log(theta[alpha_idx] + theta[alpha_idx + 1] + z)  # alpha_d + beta_d
             gradient[alpha_idx] -= vd[D][z] * ((theta[alpha_idx] + theta[alpha_idx + 1] + z) ** -1)  # alpha_d
             gradient[alpha_idx + 1] -= vd[D][z] * ((theta[alpha_idx] + theta[alpha_idx + 1] + z) ** -1)  # beta_d
-            h_diag[alpha_idx] += vd[D][z] * ((theta[alpha_idx] + theta[alpha_idx + 1] + z) ** -2)  # alpha_d
-            h_diag[alpha_idx + 1] += vd[D][z] * ((theta[alpha_idx] + theta[alpha_idx + 1] + z) ** -2)  # beta_d
+            # h_diag[alpha_idx] += vd[D][z] * ((theta[alpha_idx] + theta[alpha_idx + 1] + z) ** -2)  # alpha_d
+            # h_diag[alpha_idx + 1] += vd[D][z] * ((theta[alpha_idx] + theta[alpha_idx + 1] + z) ** -2)  # beta_d
             constants[D - 1] += vd[D][z] * ((theta[alpha_idx] + theta[alpha_idx + 1] + z) ** -2)
     return gradient, h_diag, constants, lprob
 