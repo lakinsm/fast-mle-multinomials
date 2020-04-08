b1cfea09d70744f6017fcd004ba52a23c8623e38
Steven Lakin <steven.m.lakin@gmail.com>
2020-04-08 14:35:16 -0600

    Temporary count dataset obs script

---
+++ count_dataset_observations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+import pickle
+import glob
+
+
+def load_count_files(temp_dir, dataset):
+	count_data = []
+	filepaths = ('{}/{}/{}.pickle'.format(temp_dir, dataset, x) for x in ('X', 'class_labels', 'key_idxs', 'value_idxs'))
+	for path in filepaths:
+		with open(path, 'rb') as infile:
+			count_data.append(pickle.load(infile))
+	return count_data
+
+
+if __name__ == '__main__':
+	temp_dir = sys.argv[1]
+	for fpath in glob.glob(temp_dir + '/*'):
+		dataset = fpath.split('/')[-2]
+		sys.stdout.write('{}\t{}\n'.format(dataset, fpath))
+	# X, class_labels, key_idxs, value_idxs = load_count_files()
