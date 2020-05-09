114d5431c08b0345d37739c13115e2e916609ec9
lakinsm <steven.m.lakin@gmail.com>
2020-05-09 16:50:30 -0600

    Logic to BLM MLE

--- multi_mle/blm.py
+++ multi_mle/blm.py
@@ -408,7 +408,7 @@ def blm_check_concavity(X, theta):
 
 def blm_newton_raphson2(X, U, vd, vd1, params, precompute,
                         max_steps, delta_eps_threshold, delta_lprob_threshold, label, verbose=False):
-    params0 = np.copy(params)
+    params0 = np.copy(params[:-1])
     current_lprob = -2e20
     delta_lprob = 2e20
     delta_params = 2e20
@@ -464,8 +464,10 @@ def blm_newton_raphson2(X, U, vd, vd1, params, precompute,
             # ))
     if param_escape:
         print('BLM Escape Detected! Switching to DM Distribution for class: {}'.format(label))
-        U, vd = dm.dm_precalc(X)
-        dm_params = dm.dm_newton_raphson2(X, U, vd, params0, precompute, max_steps, delta_eps_threshold,
+        U0, vd0 = (None, None)
+        if precompute == 'sklar':
+            U0, vd0 = dm.dm_precalc(X)
+        dm_params = dm.dm_newton_raphson2(X, U0, vd0, params0, precompute, max_steps, delta_eps_threshold,
                                           delta_lprob_threshold, label, verbose)
         params = np.concatenate(dm_params, np.sum(dm_params[:-1]))  # Force DM into BLM with equivalent parameters
     else:
